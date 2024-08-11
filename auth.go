13a2bcdfaa81f408e9aacce307b7194c49da99a5
maliByatzes <malib2027@gmail.com>
2024-08-11 17:17:39 +0200

    implement revelant interfaces and methods

--- auth.go
+++ auth.go
@@ -1,6 +1,14 @@
 package ocs
 
-import "time"
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+const (
+	AuthSourceGithub = "github"
+)
 
 type Auth struct {
 	ID           int        `json:"id"`
@@ -13,4 +21,42 @@ type Auth struct {
 	Expiry       *time.Time `json:"-"`
 	CreatedAt    time.Time  `json:"createdAt"`
 	UpdatedAt    time.Time  `json:"updatedAt"`
+}
+
+func (a *Auth) Error() error {
+	if a.StudentID == 0 {
+		return Errorf(EINVALID, "Student required.")
+	} else if a.Source == "" {
+		return Errorf(EINVALID, "Source required.")
+	} else if a.SourceID == "" {
+		return Errorf(EINVALID, "Source ID required.")
+	} else if a.AccessToken == "" {
+		return Errorf(EINVALID, "Access Token required.")
+	}
+	return nil
+}
+
+func (a *Auth) AvatarURL(size int) string {
+	switch a.Source {
+	case AuthSourceGithub:
+		return fmt.Sprintf("https://avatars1.githubusercontent.com/u/%s?s=%d", a.SourceID, size)
+	default:
+		return ""
+	}
+}
+
+type AuthService interface {
+	FindAuthByID(ctx context.Context, id int) (*Auth, error)
+	FindAuths(ctx context.Context, filter AuthFilter) ([]*Auth, error)
+	CreateAuth(ctx context.Context, auth *Auth) error
+	DeleteAuth(ctx context.Context, id int) error
+}
+
+type AuthFilter struct {
+	ID        *int    `json:"id"`
+	StudentID *int    `json:"studentID"`
+	Source    *string `json:"source"`
+	SourceID  *string `json:"sourceID"`
+	Offset    int     `json:"offset"`
+	Limit     int     `json:"limit"`
 }
--- student.go
+++ student.go
@@ -1,6 +1,9 @@
 package ocs
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
 type Student struct {
 	ID        int       `json:"id"`
@@ -9,5 +12,42 @@ type Student struct {
 	APIKey    string    `json:"-"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
-	Auth      []*Auth   `json:"auths"`
+	Auths     []*Auth   `json:"auths"`
+}
+
+func (s *Student) Validate() error {
+	if s.Name == "" || s.Email == "" {
+		return Errorf(EINVALID, "Provide required fields")
+	}
+	return nil
+}
+
+func (s *Student) AvatarURL(size int) string {
+	for _, auth := range s.Auths {
+		if s := auth.AvatarURL(size); s != "" {
+			return s
+		}
+	}
+	return ""
+}
+
+type StudentService interface {
+	FindStudentByID(ctx context.Context, id int) (*Student, error)
+	FindStudents(ctx context.Context, filter StudentFilter) ([]*Student, error)
+	CreateStudent(ctx context.Context, student *Student) error
+	UpdateStudent(ctx context.Context, id int, upd StudentUpdate) (*Student, error)
+	DeleteStudent(ctx context.Context, id int) error
+}
+
+type StudentFilter struct {
+	ID     *int    `json:"id"`
+	Email  *string `json:"email"`
+	APIKey *string `json:"apiKey"`
+	Offset int     `json:"offset"`
+	Limit  int     `json:"limit"`
+}
+
+type StudentUpdate struct {
+	Name  *string `json:"name"`
+	Email *string `json:"email"`
 }
