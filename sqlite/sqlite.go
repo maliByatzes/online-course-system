bfca4ee806fe17d918db084d9bc8d313c252a089
maliByatzes <malib2027@gmail.com>
2024-08-12 13:45:00 +0200

    implement the rest of db methods

--- sqlite/sqlite.go
+++ sqlite/sqlite.go
@@ -3,9 +3,11 @@ package sqlite
 import (
 	"context"
 	"database/sql"
+	"database/sql/driver"
 	"embed"
 	"fmt"
 	"io/fs"
+	"log"
 	"os"
 	"path/filepath"
 	"sort"
@@ -125,4 +127,112 @@ func (db *DB) migrateFile(name string) error {
 	return tx.Commit()
 }
 
-func (db *DB) monitor() {}
+func (db *DB) Close() error {
+	db.cancel()
+
+	if db.db != nil {
+		return db.db.Close()
+	}
+
+	return nil
+}
+
+func (db *DB) BeginTx(ctx context.Context, opts *sql.TxOptions) (*Tx, error) {
+	tx, err := db.db.BeginTx(ctx, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Tx{
+		Tx:  tx,
+		db:  db,
+		now: db.Now().UTC().Truncate(time.Second),
+	}, nil
+}
+
+func (db *DB) monitor() {
+	ticker := time.NewTicker(10 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-db.ctx.Done():
+			return
+		case <-ticker.C:
+		}
+
+		if err := db.updateStats(db.ctx); err != nil {
+			log.Printf("stats error: %s", err)
+		}
+	}
+}
+
+func (db *DB) updateStats(ctx context.Context) error {
+	tx, err := db.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	var n int
+	if err := tx.QueryRowContext(ctx, `SELECT COUNT(*) FROM students;`).Scan(&n); err != nil {
+		return fmt.Errorf("students count: %w", err)
+	}
+	studentCountGauge.Set(float64(n))
+
+	return nil
+}
+
+type Tx struct {
+	*sql.Tx
+	db  *DB
+	now time.Time
+}
+
+type NullTime time.Time
+
+func (n *NullTime) Scan(value interface{}) error {
+	if value == nil {
+		*(*time.Time)(n) = time.Time{}
+		return nil
+	} else if value, ok := value.(string); ok {
+		*(*time.Time)(n), _ = time.Parse(time.RFC3339, value)
+		return nil
+	}
+	return fmt.Errorf("NullTime: cannot scan time.Time: %T", value)
+}
+
+func (n *NullTime) Value() (driver.Value, error) {
+	if n == nil || (*time.Time)(n).IsZero() {
+		return nil, nil
+	}
+	return (*time.Time)(n).UTC().Format(time.RFC3339), nil
+}
+
+func FormatLimitOffset(limit, offset int) string {
+	if limit > 0 && offset > 0 {
+		return fmt.Sprintf(`LIMIT %d OFFSET %d`, limit, offset)
+	} else if limit > 0 {
+		return fmt.Sprintf(`LIMIT %d`, limit)
+	} else if offset > 0 {
+		return fmt.Sprintf(`OFFSET %d`, offset)
+	}
+	return ""
+}
+
+func FormatError(err error) error {
+	if err == nil {
+		return nil
+	}
+
+	// add switch case statements later
+	switch err.Error() {
+	default:
+		return err
+	}
+}
+
+func logstr(s string) string {
+	println(s)
+	return s
+}
